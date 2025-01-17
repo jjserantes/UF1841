e9535a02e89b5bcbcf13238666ff2ad75b431081
jjserantes <jserantes@gmail.com>
2025-01-17 17:49:40 +0100

    1 version

---
+++ main.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen</title>
+    <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <img src="Imagenes/logo.png" alt="logoweb">
+        </header> 
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="login.html">Login</a></li>
+                <li><a href="registro.php">Registro</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+            </ul>
+        </nav>  
+        <main>
+            <aside>
+                <section>
+                    <div class="entradas">
+                    <h2>Entradas Recientes</h2>
+                    <ul>
+                        <li>Novedades del curso 2022</li>
+                        <li>Notas de Programación A4</li>
+                        <li>Viaje Amazon</li>
+                        <li>Los últimos proyectos</li>
+                        <li>El diseño Adaptativo</li>
+                    </ul>
+                    </div>
+                </section>
+                <section>
+                    <div class="categorias">
+                    <h2>Categorías</h2>
+                    <ul>
+                        <li>Programación</li>
+                        <li>Recursos</li>
+                        <li>HTML</li>
+                    </ul>
+                    </div>
+                </section>
+            </aside>
+            <section>
+                <div class="cursos-grid">
+                    <div>
+                        <img src="Imagenes/html.png" alt="html">
+                        <h3>Cursos HTML</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div>
+                        <img src="Imagenes/css.png" alt="CSS">
+                        <h3>Cursos CSS</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div>
+                        <img src="Imagenes/js.png" alt="JS">
+                        <h3>Cursos JS</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div>
+                        <img src="Imagenes/php.png" alt="PHP">
+                        <h3>Cursos PHP</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div>
+                        <img src="Imagenes/node.png" alt="Node">
+                        <h3>Cursos NODE</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div>
+                        <img src="Imagenes/java.png" alt="Nosedejaba">
+                        <h3>Cursos JAVA</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </section>
+        </main>   
+        <footer>
+        <div class="footer"> 
+            <div class="left">NO SE DE JAVA</div> 
+            <div class="right">FORMACOM</div> 
+        </div>
+        </footer> 
+    </div>
+</body>
+</html>
